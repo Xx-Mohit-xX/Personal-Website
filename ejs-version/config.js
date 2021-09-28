61afb23704d717b5784f071522ecff5920f00fbf
Itz-Hyperz <loganfgross@gmail.com>
2021-09-27 21:13:27 -0400

    Add files via upload

---
+++ ejs-version/config.js
@@ -0,0 +1,17 @@
+const config = {
+
+    // Main Website Settings
+    port: "3000",
+    domain: "http://localhost:3000",
+
+    // Cool Customisable Stuffs
+    redirects: [
+        { name: `hyperz`, link: `https://hyperz.dev/discord` }
+    ],
+
+    // Just for a little watermark lolz
+    ogAuthor: "Hyperz#0001"
+
+}
+
+module.exports = config;
---
+++ ejs-version/index.js
@@ -0,0 +1,43 @@
+// Imports
+const express = require('express')
+const app = express()
+const chalk = require('chalk')
+const ms = require('ms')
+const nodelogger = require('hyperz-nodelogger')
+const logger = new nodelogger()
+
+const config = require('./config.js');
+const backend = require('./backend.js');
+let con;
+
+// Static Files
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+// Set View's
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+// Navigation (add pages here)
+app.get('', (req, res) => {
+    res.render('index.ejs', { backend: backend, config: config, con: con })
+})
+
+// Searched the redirects for the page (must be 1 before 404 page)
+config.redirects.forEach(element => {
+    app.get(`/${element.name}`, (req, res) => {
+        res.redirect(element.link);
+    });
+});
+
+// MAKE SURE THIS IS LAST FOR 404 PAGE REDIRECT
+app.get('*', function(req, res){
+    res.render('404.ejs', { backend: backend, config: config, con: con });
+});
+
+// Main Logger Event
+logger.hypelogger(`ExpressJS`, '500', 'blue', `Domain: ${chalk.blue(config.domain)}\nPort: ${chalk.blue(config.port)}\n\nAuthor: ${chalk.blue(config.ogAuthor)}`, 'disabled', 'blue', 'single', true)
+
+// Port Listening
+app.listen(config.port)
